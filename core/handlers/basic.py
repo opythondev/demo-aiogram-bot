d18eae529fd5aaab84b78890d442b50f442727e3
Sergey <21218903+xchakiex@users.noreply.github.com>
2023-05-17 16:50:48 +0300

    upd[basic.py]: add get_hello

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -1,3 +1,5 @@
+import json
+
 from aiogram import Bot
 from aiogram.types import Message
 import asyncio
@@ -13,3 +15,9 @@ async def get_photo(message: Message, bot: Bot):
     await message.answer(f"You send me a photo, I saved it")
     file = await bot.get_file(message.photo[-1].file_id)
     await bot.download_file(file.file_path, destination="photo.jpg")
+
+
+async def get_hello(message: Message, bot: Bot):
+    await message.answer(f"И тебе привет!")
+    json_str = json.dumps(message.dict(), default=str)
+    print(json_str)
