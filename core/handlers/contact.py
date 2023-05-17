829f9bb9fb4dd753f7cc1751a75e84d9fdb05e22
Sergey <21218903+xchakiex@users.noreply.github.com>
2023-05-17 17:56:01 +0300

    ref[contact.py]: get_true_contacts handler return additional phone

--- core/handlers/contact.py
+++ core/handlers/contact.py
@@ -2,8 +2,8 @@ from aiogram.types import Message
 from aiogram import Bot
 
 
-async def get_true_contacts(message: Message, bot: Bot):
-    await message.answer(f"ты отправил <b>свой</b> контакт")
+async def get_true_contacts(message: Message, bot: Bot, phone: str):
+    await message.answer(f"ты отправил <b>свой</b> контакт {phone}")
 
 
 async def get_fake_contacts(message: Message, bot: Bot):
