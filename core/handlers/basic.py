367497942f0bd58b704ba850c0cc66e5c2c13824
Sergey <21218903+xchakiex@users.noreply.github.com>
2023-05-17 12:47:30 +0300

    upd[basic.py]: add get_photo handler

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -7,3 +7,9 @@ async def get_start(message: Message, bot: Bot):
     await bot.send_message(message.from_user.id, f"<b>Привет! {message.from_user.first_name}</b>")
     await message.answer(f"<s>Привет! {message.from_user.first_name}</s>")
     await message.reply(f"<tg-spoiler>Привет! {message.from_user.first_name}</tg-spoiler>")
+
+
+async def get_photo(message: Message, bot: Bot):
+    await message.answer(f"You send me a photo, I saved it")
+    file = await bot.get_file(message.photo[-1].file_id)
+    await bot.download_file(file.file_path, destination="photo.jpg")
