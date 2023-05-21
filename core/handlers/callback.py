ef5d801fb4152c3440720e0f29ca22a58977293a
Sergey <21218903+xchakiex@users.noreply.github.com>
2023-05-21 14:23:13 +0300

    upd[callback.py]: select_macbook impl

---
+++ core/handlers/callback.py
@@ -0,0 +1,13 @@
+from aiogram import Bot
+from aiogram.types import CallbackQuery
+
+
+async def select_macbook(call: CallbackQuery):
+    model = call.data.split('_')[1]
+    chip = call.data.split('_')[2]
+    ram = call.data.split('_')[3]
+    year = call.data.split('_')[4]
+    answer = f'{call.message.from_user.first_name}, вы выбрали Macbook' \
+             f' модель {model}, chip {chip}, ram {ram}, year {year}'
+    await call.message.answer(answer)
+    await call.answer()
