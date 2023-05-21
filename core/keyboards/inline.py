d274e8da8d2dfaccf7ba5a83cb39728bdffeaa5b
Sergey <21218903+xchakiex@users.noreply.github.com>
2023-05-21 15:54:09 +0300

    upd[inline.py]: add mackbook_ prefix

--- core/keyboards/inline.py
+++ core/keyboards/inline.py
@@ -4,21 +4,21 @@ select_macbook = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton(
             text="Mac Book Air m1 2020 8Gb",
-            callback_data="Air_m1_2020_8Gb"
+            callback_data="macbook_Air_m1_2020_8Gb"
         ),
         InlineKeyboardButton(
             text="Mac Book Air m2 2022 16Gb",
-            callback_data="Air_m2_2022_16Gb"
+            callback_data="macbook_Air_m2_2022_16Gb"
         )
     ],
     [
         InlineKeyboardButton(
             text="Mac Book Pro m1 2020 16Gb",
-            callback_data="Pro_m1_2020_16Gb"
+            callback_data="macbook_Pro_m1_2020_16Gb"
         ),
         InlineKeyboardButton(
             text="Mac Book Pro m2 2023 16Gb",
-            callback_data="Pro_m2_2023_16Gb"
+            callback_data="macbook_Pro_m2_2023_16Gb"
         )
     ],
     [
