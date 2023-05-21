91ab81909154de1860a1982ce72bacb3f5d0191b
Sergey <21218903+xchakiex@users.noreply.github.com>
2023-05-21 13:24:42 +0300

    upd[iscontact.py]:linter updates

--- core/filters/iscontact.py
+++ core/filters/iscontact.py
@@ -3,6 +3,9 @@ from aiogram.types import Message
 
 
 class IsTrueContact(BaseFilter):
+    """
+
+    """
     async def __call__(self, message: Message):
         if message.contact.user_id == message.from_user.id:
             return {'phone': message.contact.phone_number}
