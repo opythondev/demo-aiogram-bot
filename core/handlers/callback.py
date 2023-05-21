2e3f1b7804a20089c30919af64b4efe4d320d9d4
Sergey <21218903+xchakiex@users.noreply.github.com>
2023-05-21 15:54:54 +0300

    fix[callback.py]: fix indexes

--- core/handlers/callback.py
+++ core/handlers/callback.py
@@ -2,10 +2,10 @@ from aiogram.types import CallbackQuery
 
 
 async def select_macbook(call: CallbackQuery):
-    model = call.data.split('_')[0]
-    chip = call.data.split('_')[1]
-    ram = call.data.split('_')[3]
-    year = call.data.split('_')[2]
+    model = call.data.split('_')[1]
+    chip = call.data.split('_')[2]
+    ram = call.data.split('_')[4]
+    year = call.data.split('_')[3]
     answer = f'{call.message.from_user.first_name}, вы выбрали Macbook' \
              f' модель {model}, chip {chip}, ram {ram}, year {year}'
     await call.message.answer(answer)
