0c0eea6235938637fe5e3dcd83c4875c245c2d08
Sergey <21218903+xchakiex@users.noreply.github.com>
2023-05-21 16:45:46 +0300

    upd[inline.py]: get_inline_keyboard with InlineKeyboardBuilder impl

--- core/keyboards/inline.py
+++ core/keyboards/inline.py
@@ -1,4 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
 
 select_macbook = InlineKeyboardMarkup(inline_keyboard=[
     [
@@ -32,3 +34,18 @@ select_macbook = InlineKeyboardMarkup(inline_keyboard=[
         )
     ]
 ])
+
+
+def get_inline_keyboard():
+    keyboard_builder = InlineKeyboardBuilder()
+
+    keyboard_builder.button(text="Mac Book Air m1 2020 8Gb", callback_data="macbook_Air_m1_2020_8Gb")
+    keyboard_builder.button(text="Mac Book Air m2 2022 16Gb", callback_data="macbook_Air_m2_2022_16Gb")
+    keyboard_builder.button(text="Mac Book Pro m1 2020 16Gb", callback_data="macbook_Pro_m1_2020_16Gb")
+    keyboard_builder.button(text="Mac Book Pro m2 2023 16Gb", callback_data="macbook_Pro_m2_2023_16Gb")
+    keyboard_builder.button(text="Open Site", url="https://google.com")
+    keyboard_builder.button(text="Profile", url="tg://user?id=6213341124")
+
+    keyboard_builder.adjust(2, 2, 1, 1)
+    return keyboard_builder.as_markup()
+
